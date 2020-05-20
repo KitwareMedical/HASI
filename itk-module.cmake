2b3201865f382884fd76bca04e53912acd56a344
Dženan Zukić <dzenan.zukic@kitware.com>
2020-05-20 08:49:36 -0400

    ENH: creating remote module infrastructure based on ITKModuleTemplate

---
+++ itk-module.cmake
@@ -0,0 +1,31 @@
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURRENT_DIR}/README.md" DOCUMENTATION)
+
+
+itk_module(HASI
+  DEPENDS
+    ITKSmoothing
+    ITKThresholding
+    ITKConnectedComponents
+    ITKDistanceMap
+    ITKRegionGrowing
+    ITKImageGrid
+    ITKLabelMap
+    ITKRegistrationCommon
+    ITKSpatialObjects
+    BoneEnhancement
+  COMPILE_DEPENDS
+    ITKImageSources
+  TEST_DEPENDS
+    ITKTestKernel
+    ITKMetaIO
+    ITKIONRRD
+    ITKIOTransformInsightLegacy
+    ITKIOTransformHDF5
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  EXCLUDE_FROM_DEFAULT
+  ENABLE_SHARED
+)
