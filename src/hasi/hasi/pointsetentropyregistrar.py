b7f9117ac94df900cf54392412bbb22de849c533
Tom Birdsong <tom.birdsong@kitware.com>
2021-04-01 10:50:48 -0400

    ENH: Add notebook demonstrating mesh template generation procedure

--- src/hasi/hasi/pointsetentropyregistrar.py
+++ src/hasi/hasi/pointsetentropyregistrar.py
@@ -44,37 +44,52 @@ class PointSetEntropyRegistrar(MeshToMeshRegistrar):
             ReturnBestParametersAndValue=True)
 
     def register(self,
-                 template_mesh:MeshType,
-                 target_mesh:MeshType,
+                 template_mesh:MeshType=None,
+                 template_point_set:PointSetType=None,
+                 target_mesh:MeshType=None,
+                 target_point_set:PointSetType=None,
                  filepath:str=None,
                  verbose=False,
                  metric=None,
+                 transform=None,
                  resample_rate=1.0,
                  max_iterations=MAX_ITERATIONS,
                  learning_rate=1.0,
-                 resample_from_target=False) -> (TransformType, MeshType):
+                 resample_from_target=False) -> (TransformType, PointSetType):
 
-        # Convert meshes to point sets
-        template_point_set = self.PointSetType.New(Points=template_mesh.GetPoints())
-        target_point_set = self.PointSetType.New(Points=target_mesh.GetPoints())
+        # Verify a template and target were passed in
+        if(not (template_mesh or template_point_set) or not (target_mesh or target_point_set)):
+            raise Exception('Registration requires both a template and a target!')
+
+        # Need both a mesh and a point set representing the template and the transform
+        if(not template_mesh):
+            template_mesh = self.MeshType.New(Points=template_point_set.GetPoints())
+        else:
+            template_point_set = self.PointSetType.New(Points=template_mesh.GetPoints())
+
+        if(not target_mesh):
+            target_mesh = self.MeshType.New(Points=target_point_set.GetPoints())
+        else:
+            target_point_set = self.PointSetType.New(Points=target_mesh.GetPoints())
 
         # Optionally resample to improve performance
         if(resample_rate != 1.0):
-            template_point_set = self.randomly_sample_mesh_points(mesh=template_mesh, sampling_rate=resample_rate)
-            target_point_set = self.randomly_sample_mesh_points(mesh=target_mesh, sampling_rate=resample_rate)
+            template_point_set = self.randomly_sample_point_set(point_set=template_point_set, sampling_rate=resample_rate)
+            target_point_set = self.randomly_sample_point_set(point_set=target_point_set, sampling_rate=resample_rate)
 
         # Define registration components
-        transform = self.TransformType.New()
-        transform.SetIdentity()
+        if not transform:
+            transform = self.TransformType.New()
+            transform.SetIdentity()
         
-        template_image = self.mesh_to_image(template_mesh)
+        #template_image = self.mesh_to_image(template_mesh)
         # Set physical dimensions for transform
-        fixed_physical_dimensions = list()
-        fixed_origin = list(template_image.GetOrigin())
+        #fixed_physical_dimensions = list()
+        #fixed_origin = list(template_image.GetOrigin())
 
-        for i in range(0, self.Dimension):
-            fixed_physical_dimensions.append(template_image.GetSpacing()[i] * \
-                (template_image.GetLargestPossibleRegion().GetSize()[i] - 1))
+        #for i in range(0, self.Dimension):
+        #    fixed_physical_dimensions.append(template_image.GetSpacing()[i] * \
+        #        (template_image.GetLargestPossibleRegion().GetSize()[i] - 1))
 
         if not metric:
             metric = itk.JensenHavrdaCharvatTsallisPointSetToPointSetMetricv4[self.PointSetType].New(
@@ -115,23 +130,24 @@ class PointSetEntropyRegistrar(MeshToMeshRegistrar):
 
         self.optimizer.StartOptimization()
 
-        print(f'Number of iterations run: {self.optimizer.GetCurrentIteration()}')
-        print(f'Final metric value: {self.optimizer.GetCurrentMetricValue()}')
-        print(f'Final transform position: {list(self.optimizer.GetCurrentPosition())}')
-        print(f'Optimizer scales: {list(self.optimizer.GetScales())}')
-        print(f'Optimizer learning rate: {self.optimizer.GetLearningRate()}')
+        if(verbose):
+            print(f'Number of iterations run: {self.optimizer.GetCurrentIteration()}')
+            print(f'Final metric value: {self.optimizer.GetCurrentMetricValue()}')
+            print(f'Final transform position: {list(self.optimizer.GetCurrentPosition())}')
+            print(f'Optimizer scales: {list(self.optimizer.GetScales())}')
+            print(f'Optimizer learning rate: {self.optimizer.GetLearningRate()}')
 
         moving_transform = metric.GetMovingTransform()
 
-        transformed_template_mesh = itk.transform_mesh_filter(template_mesh, transform=moving_transform)
+        registered_template_mesh = itk.transform_mesh_filter(template_mesh, transform=moving_transform)
 
         if(resample_from_target):
-            transformed_template_mesh = self.resample_template_from_target(transformed_template_mesh, target_mesh)
+            registered_template_mesh = self.resample_template_from_target(registered_template_mesh, target_mesh)
 
         # Write out
         if filepath is not None:
-            itk.meshwrite(transformed_template_mesh,filepath)
+            itk.meshwrite(registered_template_mesh,filepath)
             if(verbose):
-                print(f'Wrote resulting mesh to {filepath}')
+                print(f'Wrote resulting point set to {filepath}')
 
-        return (transform, transformed_template_mesh)
+        return (transform, registered_template_mesh)
