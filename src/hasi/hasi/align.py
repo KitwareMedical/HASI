bdfc6aada056f1f000ac72e5e74854fdf29bfd72
Tom Birdsong <tom.birdsong@kitware.com>
2021-05-10 10:38:48 -0400

    STYLE: Add NumFOCUS copyright headers to HASI Python package files

--- src/hasi/hasi/align.py
+++ src/hasi/hasi/align.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
-# Filename: align.py
-# Contributors: Tom Birdsong
+# Copyright NumFOCUS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Purpose: Python functions for mesh registration and alignment
 #           in HASI pipeline
 
